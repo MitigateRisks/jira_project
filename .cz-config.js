aa64e3155eaaef6cac45b983cb2929e0ef76710c
siyangzhang <15157147149@163.com>
2022-11-11 20:44:02 +0800

    feat(custom): git

---
+++ .cz-config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  //可选类型
+  types: [
+    { value: 'feat', name: 'feat:   新功能' },
+    { value: 'fix', name: 'fix:   修复' },
+    { value: 'docs', name: 'docs:   文档变更' },
+    { value: 'style', name: 'style:   代码格式(不影响代码运行的变动)' },
+    {
+      value: 'refactor',
+      name: 'refactor:   重构(既不是增加feature,也不是修复bug)'
+    },
+    { value: 'pref', name: 'pref:   性能优化' },
+    { value: 'test', name: 'test:   增加测试' },
+    { value: 'chore', name: 'chore:   构建过程或辅助工具的变动' },
+    { value: 'revert', name: 'revert:   回退' },
+    { value: 'build', name: 'build:   打包' },
+  ],
+  //步骤
+  messages: {
+    type: '请选择提交的类型',
+    customScope: '请输入修改的范围(可选)',
+    subject: '请简要描述提交(必填)',
+    body: '请输入详细描述(可选)',
+    footer: '请输入要关闭的issue(可选)',
+    confirmCommit: '请 确认使用以上信息提交?(y/n)'
+  },
+  //跳过步骤
+  skipQuestions: ['body', 'footer'],
+  //设置长度限制 默认为72
+  subjectLimit: 72
+}
